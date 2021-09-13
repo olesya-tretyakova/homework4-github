e55c22be11e1508d202f1a9926711405fd68c107
Olesya Tretyakova <OTretyakova@beeline.kz>
2021-09-13 09:53:48 +0600

    add GitHubTest

---
+++ src/test/java/com/my_company/GitHubTest.java
@@ -0,0 +1,31 @@
+package com.my_company;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class GitHubTest {
+    @Test
+    void softAssertionPageHasJunitFiveCode() {
+        //Открыть страницу Selenide в Github
+        open("https://github.com/selenide/selenide");
+
+        //Перейти в раздел Wiki проекта
+        $("#wiki-tab").click();
+
+        //Ввести в поле фильтра 'SoftAssertions'
+        $("#wiki-pages-filter").setValue("SoftAssertions");
+
+        //Убедиться, что в списке страниц (Pages) есть страница SoftAssertions
+        $("[data-filterable-for=wiki-pages-filter]").shouldHave(text("SoftAssertions"));
+
+        // Открыть страницу SoftAssertions
+        $("[data-filterable-for=wiki-pages-filter]").$(byText("SoftAssertions")).click();
+
+        //Проверить ,что внутри есть пример кода для JUnit5
+        $("#wiki-body").shouldHave(text("JUnit5"));
+    }
+}
